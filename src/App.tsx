f4a1312ffb241333502233f5662853d815cd0f53
HaillSeitan <150841231+HaillSeitan@users.noreply.github.com>
2024-07-09 09:30:23 +0200

    added Name and Age input

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,57 @@
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+// Improting ZOD
+//install with: npm install zod,
 import { z } from "zod";
+//
+// Importing React Hook Form
+import { useForm } from "react-hook-form";
+//
+//install with: npm install @hookform/resolvers
+import { zodResolver } from "@hookform/resolvers/zod";
 
 const signUpSchema = z
   .object({
-    nameFull: z.string().includes(" "),
-    birthday: z.date(),
+    // Validates if input is string, conditions: between 3-25 charakters log, disabeled special characters
+    userName: z
+      .string()
+      .min(3, "Username must be at least 3 characters long")
+      .max(25, "Username must not be greater than 25 characters")
+      .regex(
+        /^[a-zA-Z0-9_]+$/,
+        "Only letters, numbers and underscore (_) allowed"
+      ),
+    // E-Mail validation based on HTML5 email input pattern (covers a wide range of valid email addresses)
     email: z.string().email(),
+    //
+    fullName: z
+      .string()
+      .min(3, "Name must be at least 3 characters long")
+      .trim()
+      .refine(
+        (fullName) => {
+          return String(fullName).includes(" ");
+        },
+        { message: "First and last name needed" }
+      ),
+    // checking if age min 18
+    age: z.number().refine(
+      (age) => {
+        return Number(age) >= 18;
+      },
+      { message: "You must be 18 years or older" }
+    ),
+    // Minimum length condition + custom error message
     password: z.string().min(10, "Password must be at least 10 characters"),
     confirmPassword: z.string(),
   })
+  // Define custom validation check (Password match)
   .refine((data) => data.password === data.confirmPassword, {
-    message: "Passwords must match",
+    message: "Try again. Passwords must match",
+    // Path of error
     path: ["confirmPassword"],
   });
+//
 
-//define type for type safty when
+//extract the inferret type - later stored in types.ts
 type SignUpSchema = z.infer<typeof signUpSchema>;
 
 export function RegisterForm() {
@@ -43,20 +79,14 @@ export function RegisterForm() {
         </h1>
         <div className="flex flex-col gap-y-3">
           <input
-            {...register("nameFull")}
+            {...register("userName")}
             type="input"
-            placeholder="Full Name"
+            placeholder="User Name"
             className="px-4 py-2 rounded-md focus:outline-none focus:ring focus:ring-zinc-400"
           />
-          {errors.nameFull && (
-            <p className="text-rose-600 text-xs">{`${errors.nameFull.message}`}</p>
+          {errors.userName && (
+            <p className="text-rose-600 text-xs">{`${errors.userName.message}`}</p>
           )}
-          <input
-            {...register("birthday")}
-            type="date"
-            placeholder="Birthday"
-            className="px-4 py-2 rounded-md focus:outline-none text-zinc-600 focus:ring focus:ring-zinc-400"
-          />
           <input
             {...register("email")}
             type="email"
@@ -65,6 +95,24 @@ export function RegisterForm() {
           />
           {errors.email && (
             <p className="text-rose-600 text-xs">{`${errors.email.message}`}</p>
+          )}
+          <input
+            {...register("fullName")}
+            type="input"
+            placeholder="Full Name"
+            className="px-4 py-2 rounded-md focus:outline-none text-zinc-600 focus:ring focus:ring-zinc-400"
+          />
+          {errors.fullName && (
+            <p className="text-rose-600 text-xs">{`${errors.fullName.message}`}</p>
+          )}
+          <input
+            {...register("age")}
+            type="input"
+            placeholder="Age"
+            className="px-4 py-2 rounded-md focus:outline-none text-zinc-600 focus:ring focus:ring-zinc-400"
+          />
+          {errors.age && (
+            <p className="text-rose-600 text-xs">{`${errors.age.message}`}</p>
           )}
           <input
             {...register("password")}
